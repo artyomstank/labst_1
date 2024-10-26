e1e35e54cc620e15370045238d7c6feba28ad385
artyomstank <art3mii411@gmail.com>
2024-10-26 20:06:46 +0300

    add json controller file

---
+++ JSONcontrol.py
@@ -0,0 +1,39 @@
+import json
+
+class DisplayJson:
+
+    def save_to_json(data, filename):
+        try:#Обработка встроенной ошибки
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+            print(f"Все блюда успешно сохранены в JSON-файл {filename}")
+        except IOError as e:
+            print(f"Ошибка при записи в JSON-файл: {str(e)}")
+
+    def load_from_json(filename):
+        try: #Обработка встроенной ошибки
+            with open(filename, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {"tragedy": [], "ScienceFictions": [], "Fairytales": [], "Fantasys": [],
+                    "Adventures": [], "CrimeAndMysterys":[]}
+
+
+    def add_tragedy(data, tragedys):
+        data['tragedys'].append(tragedys.to_dict())
+
+    def add_Science_Fiction(data, sf):
+        data['drinks'].append(sf.to_dict())
+
+    def add_Fairytale(data, frt):
+        data['alcohols'].append(frt.to_dict())
+
+    def add_Fantasy(data, fnt):
+        data['desserts'].append(fnt.to_dict())
+
+    def add_Adventure(data, adv):
+        data['salads'].append(adv.to_dict())
+
+    def add_CrimeAndMystery(data, cam):
+        data['bakeries'].append(cam.to_dict())
+
